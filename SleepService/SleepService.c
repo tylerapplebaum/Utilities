9c6e0179d37dd3ee64976f10a10f6571d179219a
Tyler Applebaum <tylerapplebaum@gmail.com>
2017-08-13 16:38:56 -0700

    Added VS2017 compatibility
    Compiled cosmonautdreams' dummy service with VS 2017. Had to add advapi32.lib and uppercase the sleep function to Sleep.

---
+++ SleepService/SleepService.c
@@ -0,0 +1,124 @@
+// http://www.cosmonautdreams.com/2015/01/13/Create-a-dummy-windows-service.html
+#include <windows.h>
+#include <stdio.h>
+#pragma comment(lib, "advapi32.lib")
+#define LOGFILE "C:\\Temp\\sleepstatus.txt"
+ 
+SERVICE_STATUS ServiceStatus; 
+SERVICE_STATUS_HANDLE hStatus; 
+  
+void  ServiceMain(int argc, char** argv); 
+void  ControlHandler(DWORD request); 
+int InitService();
+ 
+int WriteToLog(char* str)
+{
+    FILE* log;
+    log = fopen(LOGFILE, "a+");
+    if (log == NULL)
+        return -1;
+    fprintf(log, "%s\n", str);
+    fclose(log);
+    return 0;
+}
+ 
+void main() 
+{ 
+    SERVICE_TABLE_ENTRY ServiceTable[2];
+    ServiceTable[0].lpServiceName = "SleepService";
+    ServiceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)ServiceMain;
+ 
+    ServiceTable[1].lpServiceName = NULL;
+    ServiceTable[1].lpServiceProc = NULL;
+    // Start the control dispatcher thread for our service
+    StartServiceCtrlDispatcher(ServiceTable);  
+}
+ 
+void ServiceMain(int argc, char** argv) 
+{ 
+    int error; 
+  
+    ServiceStatus.dwServiceType        = SERVICE_WIN32; 
+    ServiceStatus.dwCurrentState       = SERVICE_START_PENDING; 
+    ServiceStatus.dwControlsAccepted   = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+    ServiceStatus.dwWin32ExitCode      = 0; 
+    ServiceStatus.dwServiceSpecificExitCode = 0; 
+    ServiceStatus.dwCheckPoint         = 0; 
+    ServiceStatus.dwWaitHint           = 0; 
+  
+    hStatus = RegisterServiceCtrlHandler(
+        "SleepService", 
+        (LPHANDLER_FUNCTION)ControlHandler); 
+    if (hStatus == (SERVICE_STATUS_HANDLE)0) 
+    { 
+        // Registering Control Handler failed
+        return; 
+    }  
+    // Initialize Service 
+    error = InitService(); 
+    if (error) 
+    {
+        // Initialization failed
+        ServiceStatus.dwCurrentState       = SERVICE_STOPPED; 
+        ServiceStatus.dwWin32ExitCode      = -1; 
+        SetServiceStatus(hStatus, &ServiceStatus); 
+        return; 
+    } 
+    // We report the running status to SCM. 
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING; 
+    SetServiceStatus (hStatus, &ServiceStatus);
+  
+    // The worker loop of a service
+    while (ServiceStatus.dwCurrentState == SERVICE_RUNNING)
+    {
+        int result;
+     
+        /* Do nothing but loop once every 5 minutes */
+        while(1)
+        {
+            Sleep(300);         
+        }
+         
+    }
+    return; 
+}
+  
+// Service initialization
+int InitService() 
+{ 
+    int result;
+    result = WriteToLog("Monitoring started.");
+    return(result); 
+} 
+ 
+ 
+// Control handler function
+void ControlHandler(DWORD request) 
+{ 
+    switch(request) 
+    { 
+        case SERVICE_CONTROL_STOP: 
+             WriteToLog("Monitoring stopped.");
+ 
+            ServiceStatus.dwWin32ExitCode = 0; 
+            ServiceStatus.dwCurrentState  = SERVICE_STOPPED; 
+            SetServiceStatus (hStatus, &ServiceStatus);
+            return; 
+  
+        case SERVICE_CONTROL_SHUTDOWN: 
+            WriteToLog("Monitoring stopped.");
+ 
+            ServiceStatus.dwWin32ExitCode = 0; 
+            ServiceStatus.dwCurrentState  = SERVICE_STOPPED; 
+            SetServiceStatus (hStatus, &ServiceStatus);
+            return; 
+         
+        default:
+            break;
+    }  
+     
+    // Report current status
+    SetServiceStatus (hStatus,  &ServiceStatus);
+  
+    return; 
+}   
