5e59ffd268999a7474eac7bab30154f9a1ca71ac
Tyler Applebaum <tylerapplebaum@gmail.com>
2017-11-10 12:19:38 -0800

    Broke out B64 functions - fixed hyphenated syntax to match pwsh standards

--- Test.ps1
+++ Test.ps1
@@ -16,7 +16,7 @@ for($i=0; $i -lt $bytes.count ; $i++) {
 #https://stackoverflow.com/questions/8908287/base64-encoding-in-python-3
 # https://stackoverflow.com/questions/35334928/convert-base64-string-to-file
 
-Function Convert-ToBase64 {
+Function ConvertTo-Base64 {
 param(
 [CmdletBinding()]
 	[Parameter(ValueFromPipeline=$True,Mandatory=$True,HelpMessage="String to encode to base64")]
@@ -27,8 +27,19 @@ $StringToByteArr = [System.Text.Encoding]::UTF8.GetBytes($String) #Converts stri
 $Base64String = [System.Convert]::ToBase64String($StringToByteArr) #Converts byte array to b64 string
 
 Return $Base64String
-$Base64ToUTF8 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Base64String)) #Converts b64 string to UTF-8 string
 } #End Convert-ToBase64
+
+Function ConvertFrom-Base64 {
+param(
+[CmdletBinding()]
+	[Parameter(ValueFromPipeline=$True,Mandatory=$True,HelpMessage="String to decode from base64")]
+	[ValidateNotNullOrEmpty()]
+    $String
+)
+$Base64ToUTF8 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($String)) #Converts b64 string to UTF-8 string
+
+Return $Base64ToUTF8
+}
 ####
 
 Function Get-StringHash {
@@ -51,7 +62,7 @@ Return $Hash
 
 ####
 
-Function Convert-ToGZip {
+Function ConvertTo-GZip {
 #Compress
 $InputString = @"
 YourInputHere2
@@ -64,7 +75,7 @@ $StreamWriter.Close()
 $GZip = [System.Convert]::ToBase64String($MemoryStream.ToArray())
 }
 
-Function Convert-FromGZip {
+Function ConvertFrom-GZip {
 #Decompress
 $data = [System.Convert]::FromBase64String($GZIP)
 $ms = New-Object System.IO.MemoryStream
